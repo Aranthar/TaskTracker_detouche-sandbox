77c13d51edda8b0fffc0ea6d66b668903e9b1cb8
Aranthar <dvkolomiets@gmail.com>
2023-11-10 03:03:24 +0500

    Добавить типографику

--- app/src/main/java/com/example/tasktracker/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/tasktracker/screens/SignUpScreen.kt
@@ -1,13 +1,19 @@
 package com.example.tasktracker.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tasktracker.ui.theme.fontFamily
 
 @Composable
 fun SignUpScreen() {
@@ -16,7 +22,7 @@ fun SignUpScreen() {
             .fillMaxSize()
             .padding(32.dp),
     ) {
-        Text(text = "Hello World!")
+
     }
 }
 
--- app/src/main/java/com/example/tasktracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/tasktracker/ui/theme/Type.kt
@@ -4,7 +4,9 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.example.tasktracker.R
 
@@ -14,29 +16,35 @@ val provider = GoogleFont.Provider(
     certificates = R.array.com_google_android_gms_fonts_certs
 )
 
+val fontText = GoogleFont("Manrope")
+
+val fontFamily = FontFamily(
+    Font(
+        googleFont = fontText,
+        fontProvider = provider,
+    )
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 27.sp,
+        lineHeight = 36.88.sp,
+        letterSpacing = 0.sp,
+        textAlign = TextAlign.Center
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 15.sp,
+        lineHeight = 20.49.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.39.sp,
     )
-    */
 )
